5d2911ffcd7042def129bca7d4ae74e52967bea2
Sara <saraheinin@outlook.com>
2019-10-22 12:14:49 +0200

    Moviequotes och slideshow

--- test/src/App.js
+++ test/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-
 import Header from "./Components/Header";
+import Slideshow from './Components/Slideshow/Slideshow';
+import MovieQuotes from './Components/MovieQuotes/MovieQuotes';
 
-function App() {
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {showPage:""};
+  }
+render(){
   return (
     <>
-    <Header/>
-            </>
-
-  );
+    <Header pageToShow={page => this.setState({showPage: page})}/>
+    {this.state.showPage}
+    <Slideshow />
+    <MovieQuotes />
+     </>
+            
+  )
+};
 }
 
 export default App;
